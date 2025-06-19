0bf119cc2ddbea7180b90c44a2fdeb2c22d96101
Bastian Velasquez <basti14velasquez@gmail.com>
2025-06-19 14:10:23 -0400

    fix duplicado de seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,15 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
-    public function run(): void
+
+    public function run()
     {
+        DB::table('admins')->where('rut', '209950456')->delete();
+
         Admin::create([
             'rut' => '209950456',
-            'contrasena' => Hash::make('admin123qr6(PS?0QWp6'),
+            'contrasena' => bcrypt('123456'),
         ]);
     }
+    
 }
